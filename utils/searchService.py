62447fc1a0b7346adb753106bb5ceb402e064603
adityaverma125 <163313193+adityaverma125@users.noreply.github.com>
2024-03-18 18:51:37 +0530

    Add files via upload

---
+++ utils/searchService.py
@@ -0,0 +1,59 @@
+from google.cloud import storage
+from pathlib import Path
+from google.cloud import storage
+from pathlib import Path
+import os
+
+path_to_private_key = 'utils/fifth-compass-415612-76f634511b19.json'  
+
+def getResumeTestData(inputPath, str_folder_name_on_gcs, localPath, privateKeyPath):
+    bucketName = inputPath.split('/')[-1]
+    client = storage.Client.from_service_account_json(json_credentials_path=privateKeyPath)
+    bucket = storage.Bucket(client, bucketName)
+    # Create the directory locally
+    Path(os.path.join(localPath, str_folder_name_on_gcs)).mkdir(parents=True, exist_ok=True)
+
+    blobs = bucket.list_blobs(prefix=str_folder_name_on_gcs)
+    for blob in blobs:
+        try:
+            if not blob.name.endswith('/'):
+                # This blob is not a directory!
+                print(blob.name)
+                print(f'Downloading file [{blob.name}]')
+                blob.download_to_filename(f'{localPath}/{blob.name}')
+        except:
+            print("Unable to Download")
+    return str_folder_name_on_gcs
+
+
+def getJDTestData(inputPath, str_folder_name_on_gcs, localPath, privateKeyPath):
+    bucketName = inputPath.split('/')[-1]
+    client = storage.Client.from_service_account_json(json_credentials_path=privateKeyPath)
+    bucket = storage.Bucket(client, bucketName)
+    # Create the directory locally
+    Path(os.path.join(localPath, str_folder_name_on_gcs)).mkdir(parents=True, exist_ok=True)
+
+    blobs = bucket.list_blobs(prefix=str_folder_name_on_gcs)
+    for blob in blobs:
+        try:
+            blobName = blob.name.split('/')
+            if len(blobName) == 2:
+                # This blob is not a directory!
+                print(f'Downloading file [{blob.name}]')
+                blob.download_to_filename(f'{localPath}/{blob.name}')
+        except:
+            print("Unable to Download")
+                    
+    return str_folder_name_on_gcs
+
+def obtain_test_data(category, localFolder, inputPath):
+    if category == "resume":
+        Path(localFolder).mkdir(parents=True, exist_ok=True)
+        getResumeTestData(inputPath, "RESUME/data", localFolder, path_to_private_key)
+    elif category == "job":
+        Path(localFolder).mkdir(parents=True, exist_ok=True)
+        getJDTestData(inputPath, "JD/", localFolder, path_to_private_key)
+    else:
+        #return jsonify({"status": "Bad Request Please check category should be either resume or job!"})
+        return "Record not found", 400
+# obtain_test_data("resume", "data/resumes", "https://console.cloud.google.com/storage/browser/hackathon1415")
